8bbf80d1ddca65ca009e7503bdd21da87d66d47d
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2022-05-01 12:40:40 +0300

    Change target framework to .NET 6. Add skeleton and test project.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Create table of records type of T
 
-- Для набора элементов типа `T` (`ICollection<T>`), где состояние каждого объекта типа `T` описывается публичными свойствами, имеющих только build-in тип реализовать возможность записи  в табличном виде  в текстовый поток.
-- Количество столбцов в таблице определяется количеством публичных свойств каждой записи. Ширина каждого столбца определяется максимальным количеством символов,  необходимым для записи в поток отдельного свойства. При этом, числа  и даты должны быть выровнены по правому краю, строки и символы - по левому. 
+- Implement the ability to write in table form to the text stream a set of elements of type `T` (`ICollection<T>`), where the state of each object of type `T` is described by public properties that have only build-in type (`int`, `char`, `string` etc.).
+- The number of columns in the table is determined by the number of public properties of each entry. The width of each column is determined by the maximum number of characters required to write an individual property to the stream. In this case, numbers and dates should be right-aligned, strings and symbols - left-aligned.
 
 
-    ![](/Records.png)
+    ![](/TableRecords.png)
 
 
-- Проверить работу разработанной функциональности на примере любого класса, обладающего обозначенным выше набором свойств, используя вывод в текстовый файл и консоль.
+- Check the operation of the developed functionality on the example of any class that has the set of properties indicated above, using the output to a text file and the console (unit tests).
--- TableOfRecords.Tests/TableOfRecordsCreatorTests.cs
+++ TableOfRecords.Tests/TableOfRecordsCreatorTests.cs
@@ -1,10 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using NUnit.Framework;
+using TableOfRecords.Tests.UserProfiles;
 
 namespace TableOfRecords.Tests
 {
     [TestFixture]
     public class TableOfRecordsCreatorTests
     {
-        //TODO: Add test methods here. Remove this comment.
+        private const string path = "Table.txt";
+
+        [Test]
+        public void WriteTable_With_ProfileBasic_Class()
+        {
+            using var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            TableOfRecordsCreator.WriteTable(TestCasesDataSource.ProfileBasic, stringWriter);
+            var streamWriter = new StreamWriter(path);
+            TableOfRecordsCreator.WriteTable(TestCasesDataSource.ProfileBasic, streamWriter);
+            streamWriter.Close();
+            var streamReader = new StreamReader(path);
+            Assert.AreEqual(stringWriter.ToString(), streamReader.ReadToEnd());
+            streamReader.Close();
+            File.Delete(path);
+        }
+
+        [Test]
+        public void WriteTable_With_ProfileExtended_Class()
+        {
+            using var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            TableOfRecordsCreator.WriteTable(TestCasesDataSource.ProfileExtended, stringWriter);
+            var streamWriter = new StreamWriter(path);
+            TableOfRecordsCreator.WriteTable(TestCasesDataSource.ProfileExtended, streamWriter);
+            streamWriter.Close();
+            var streamReader = new StreamReader(path);
+            Assert.AreEqual(stringWriter.ToString(), streamReader.ReadToEnd());
+            streamReader.Close();
+            File.Delete(path);
+        }
+
+        [Test]
+        public void WriteTable_With_ProfileShort_Class()
+        {
+            using var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            TableOfRecordsCreator.WriteTable(TestCasesDataSource.ProfileShort, stringWriter);
+            var streamWriter = new StreamWriter(path);
+            TableOfRecordsCreator.WriteTable(TestCasesDataSource.ProfileShort, streamWriter);
+            streamWriter.Close();
+            var streamReader = new StreamReader(path);
+            Assert.AreEqual(stringWriter.ToString(), streamReader.ReadToEnd());
+            streamReader.Close();
+            File.Delete(path);
+        }
+
+        [Test]
+        public void WriteTable_With_ProfileShort_And_Console()
+        {
+            using var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            TableOfRecordsCreator.WriteTable(TestCasesDataSource.ProfileShortCollection, stringWriter);
+            Assert.AreEqual(TestCasesDataSource.ProfileShortTable, stringWriter.ToString());
+        }
+
+        [Test]
+        public void WriteTable_With_ProfileBasic_And_Console()
+        {
+            using var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            TableOfRecordsCreator.WriteTable(TestCasesDataSource.ProfileBasicCollection, stringWriter);
+            Assert.AreEqual(TestCasesDataSource.ProfileBasicTable, stringWriter.ToString());
+        }
+
+        [Test]
+        public void WriteTable_With_ProfileExtended_And_Console()
+        {
+            using var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            TableOfRecordsCreator.WriteTable(TestCasesDataSource.ProfileExtendedCollection, stringWriter);
+            Assert.AreEqual(TestCasesDataSource.ProfileExtendedTable, stringWriter.ToString());
+        }
+
+        [Test]
+        public void WriteTable_If_Collection_Is_Null_Throw_ArgumentNullException() =>
+            Assert.Throws<ArgumentNullException>(() =>
+                TableOfRecordsCreator.WriteTable<ProfileBasic>(null!, Console.Out));
+
+        [Test]
+        public void WriteTable_If_Collection_Is_Empty_Throw_ArgumentException() =>
+            Assert.Throws<ArgumentException>(() =>
+                TableOfRecordsCreator.WriteTable(Array.Empty<ICollection<ProfileBasic>>(), Console.Out));
+
+        [Test]
+        public void WriteTable_If_TextWriter_Is_Null_Throw_ArgumentNullException()
+            => Assert.Throws<ArgumentNullException>(() =>
+                TableOfRecordsCreator.WriteTable(TestCasesDataSource.ProfileBasic, null));
     }
 }
